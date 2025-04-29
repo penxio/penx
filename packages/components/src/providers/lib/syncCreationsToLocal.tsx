2aa10b1723f3b9606c9c876c5622bed457bf0782
0xzio <zio.penx@gmail.com>
2025-04-29 13:23:09 +0800

    feat: improve sync

--- packages/components/src/providers/lib/syncCreationsToLocal.tsx
+++ packages/components/src/providers/lib/syncCreationsToLocal.tsx
@@ -15,14 +15,15 @@ import { SHAPE_URL } from '@penx/constants'
 import { refetchCreations } from '@penx/hooks/useCreations'
 import { localDB } from '@penx/local-db'
 import { queryClient } from '@penx/query-client'
+import { AsyncQueue } from './AsyncQueue'
 import { isRowsEqual } from './isRowsEqual'
 import { getElectricSyncState, setElectricSyncState } from './syncState'
+
+const queue = new AsyncQueue()
 
 export async function syncCreationsToLocal(siteId: string) {
   const { last_lsn, ...metadata } = await getElectricSyncState('creation')
   // console.log('========syncState:', metadata, 'last_lsn:', last_lsn)
-
-  console.log('===========SHAPE_URL:', SHAPE_URL)
 
   const stream = new ShapeStream({
     url: SHAPE_URL,
@@ -35,80 +36,81 @@ export async function syncCreationsToLocal(siteId: string) {
 
   const shape = new Shape(stream)
 
-  shape.subscribe(async ({ rows }) => {
-    console.log('>>>>>>>>==creation row:', rows)
-  })
+  // shape.subscribe(async ({ rows }) => {
+  //   console.log('>>>>>>>>==creation row:', rows)
+  // })
 
   stream.subscribe(async (messages) => {
-    console.log('=======>>>>>>messages:', messages)
+    queue.addTask(() => sync(siteId, stream, messages))
+  })
+}
 
-    const { changes, lsn } = handleMessages(messages)
+async function sync(
+  siteId: string,
+  stream: ShapeStream<Row<never>>,
+  messages: Message<Row<never>>[],
+) {
+  console.log('=======>>>>>>messages:', messages)
 
-    console.log('========changes:', changes, 'lsn:', lsn)
-    if (!changes.length) return
-    const state = await getElectricSyncState('creation')
+  const { changes, lsn } = handleMessages(messages)
 
-    if (lsn && state?.last_lsn && BigInt(lsn) <= BigInt(state.last_lsn)) {
-      console.log(
-        'no need to sync, skipping...',
-        'lsn:',
-        lsn,
-        'state.last_lsn:',
-        state.last_lsn,
-      )
-      return
-    }
+  console.log('========changes:', changes, 'lsn:', lsn)
+  if (!changes.length) return
+  const state = await getElectricSyncState('creation')
 
-    try {
-      let updated = false
-      const creations = await localDB.creation.where({ siteId }).toArray()
-      await localDB.transaction('rw', localDB.creation, async () => {
-        for (const message of changes) {
-          const value = message.value as any
-          const operation = message.headers.operation
-          if (operation === 'insert') {
-            // console.log('insert:', message)
-            await localDB.creation.put(value)
-            updated = true // TODO:
-          }
-          if (operation === 'update') {
-            const creation = creations.find((c) => c.id === value.id)
-            const changed = Object.keys(value)
-              .filter((k) => k !== 'updatedAt')
-              .some((key) => {
-                if (!creation) return true
-                // console.log('=====value[key]:', value[key], creation[key])
-                return !isEqual(value[key], (creation as any)[key])
-              })
+  if (lsn && state?.last_lsn && BigInt(lsn) <= BigInt(state.last_lsn)) {
+    return
+  }
 
-            if (changed) {
-              await localDB.creation.update(value.id, value)
-              updated = true
-            }
-          }
-          if (operation === 'delete') {
-            // console.log('message delete:', message)
-            await localDB.creation.delete(value.id)
-            updated = true // TODO:
+  try {
+    let updated = false
+    const creations = await localDB.creation.where({ siteId }).toArray()
+    await localDB.transaction('rw', localDB.creation, async () => {
+      for (const message of changes) {
+        const value = message.value as any
+        const operation = message.headers.operation
+        if (operation === 'insert') {
+          // console.log('insert:', message)
+          await localDB.creation.put(value)
+          updated = true // TODO:
+        }
+        if (operation === 'update') {
+          const creation = creations.find((c) => c.id === value.id)
+          const changed = Object.keys(value)
+            .filter((k) => k !== 'updatedAt')
+            .some((key) => {
+              if (!creation) return true
+              // console.log('=====value[key]:', value[key], creation[key])
+              return !isEqual(value[key], (creation as any)[key])
+            })
+
+          if (changed) {
+            await localDB.creation.update(value.id, value)
+            updated = true
           }
         }
-      })
-
-      console.log('synced:', updated)
-
-      await setElectricSyncState('creation', {
-        handle: stream.shapeHandle!,
-        offset: stream.lastOffset,
-        last_lsn: lsn,
-      })
-
-      if (updated) {
-        refetchCreations()
+        if (operation === 'delete') {
+          // console.log('message delete:', message)
+          await localDB.creation.delete(value.id)
+          updated = true // TODO:
+        }
       }
-    } catch (error) {
-      console.error(error)
+    })
+
+    console.log('synced:', updated)
+
+    await setElectricSyncState('creation', {
+      handle: stream.shapeHandle!,
+      offset: stream.lastOffset,
+      last_lsn: lsn,
+    })
+
+    if (updated) {
+      await refetchCreations()
     }
-  })
+  } catch (error) {
+    console.error(error)
+  }
 }
 
 function handleMessages(messages: Message<Row<never>>[], debug = true) {
