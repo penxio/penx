f80c9db39649fc1df1c4285828401f63463497e1
0xzio <zio.penx@gmail.com>
2025-05-06 23:00:16 +0800

    feat: improve sync

--- packages/worker/src/pollingSyncToRemote.ts
+++ packages/worker/src/pollingSyncToRemote.ts
@@ -1,29 +1,24 @@
 import { get, set } from 'idb-keyval'
+import { isDesktop, ROOT_HOST } from '@penx/constants'
 import { localDB } from '@penx/local-db'
 import { api } from '@penx/trpc-client'
 import { SessionData } from '@penx/types'
 import { sleep } from '@penx/utils'
 
 export async function pollingSyncToRemote() {
-  let pollingInterval = 3 * 1000
+  let pollingInterval = 5 * 1000
 
   // console.log('=======pollingInterval:', pollingInterval)
 
   while (true) {
-    console.log('sync to remote')
-
-    try {
-      await sync()
-    } catch (error) {
-      console.log('======error:', error)
-    }
+    console.log('sync to remote...........isDesktop:', isDesktop)
+    await sync()
     await sleep(pollingInterval)
   }
 }
 
 async function sync() {
   const session = (await get('SESSION')) as SessionData
-  console.log('=======session polling:', session)
 
   if (!session || !session?.siteId) return
 
@@ -34,7 +29,6 @@ async function sync() {
   const changes = await localDB.change
     .where({ siteId: session.siteId, synced: 0 })
     .sortBy('id')
-  console.log('=======changes:', changes)
 
   for (const change of changes) {
     if (change.synced) continue
@@ -46,8 +40,36 @@ async function sync() {
       key: change.key,
       data: change.data,
     }
-    await api.sync.push.mutate(data)
-    // await localDB.change.update(change.id, { synced: 1 })
-    await localDB.change.delete(change.id)
+
+    try {
+      // fetch('https://jsonplaceholder.typicode.com/todos/1')
+      //   .then((response) => response.json())
+      //   .then((json) => console.log(json))
+
+      fetch(`${ROOT_HOST}/api/v1/sync`)
+        .then((response) => response.json())
+        .then((json) => console.log(json))
+
+      // await api.sync.push.mutate(data)
+
+      let headers: Record<string, string> = {}
+
+      if (isDesktop) {
+        const session = await get('SESSION')
+        if (session?.accessToken) {
+          headers.Authorization = session.accessToken
+        }
+      }
+
+      await fetch(`${ROOT_HOST}/api/v1/sync`, {
+        method: 'POST',
+        headers,
+        body: JSON.stringify(data),
+      })
+      // await localDB.change.update(change.id, { synced: 1 })
+      await localDB.change.delete(change.id)
+    } catch (error) {
+      console.log('error syncing change:', error)
+    }
   }
 }
