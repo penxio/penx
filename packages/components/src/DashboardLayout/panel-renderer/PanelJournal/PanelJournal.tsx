552f7625490ba484fd2616fd812c5ac2519cfdb8
0xjojo1 <jojomemo5129@gmail.com>
2025-07-03 14:06:41 +0800

    fix(PanelJournal): resolve alignment issue between JournalContent and JournalQuickInput

--- packages/components/src/DashboardLayout/panel-renderer/PanelJournal/PanelJournal.tsx
+++ packages/components/src/DashboardLayout/panel-renderer/PanelJournal/PanelJournal.tsx
@@ -39,32 +39,31 @@ export function PanelJournal({ panel, index }: Props) {
       </PanelHeaderWrapper>
 
       <div className="relative flex-1">
-        <div className="absolute bottom-0 left-0 right-0 top-0  flex h-full flex-1 flex-col">
-          <div
-            className={cn(
-              'relative z-50 flex w-full flex-1 flex-col overflow-auto px-6 pb-20 pt-6',
-            )}
-          >
-            <JournalContent />
-          </div>
-          {!isMobileApp && (
-            <div className="relative z-50 px-6 pb-6">
-              <div className="sticky bottom-0 mx-auto flex w-full max-w-xl flex-col items-center justify-center gap-2">
-                <JournalQuickInput />
-              </div>
+        <div className="absolute bottom-0 left-0 right-0 top-0 flex h-full flex-1 flex-col overflow-auto px-6">
+          <div className="relative z-50 flex flex-1 flex-col pt-6">
+            <div className="flex-1 pb-6">
+              <JournalContent />
             </div>
-          )}
 
-          <div
-            className="absolute bottom-0 left-0 right-0 top-0 z-10 opacity-30 dark:opacity-0"
-            style={{
-              filter: 'blur(150px) saturate(150%)',
-              transform: 'translateZ(0)',
-              backgroundImage:
-                'radial-gradient(at 27% 37%, #eea5ba 0, transparent 50%), radial-gradient(at 97% 21%, #fd3a4e 0, transparent 50%), radial-gradient(at 52% 99%, #e4c795 0, transparent 50%), radial-gradient(at 10% 29%, #5afc7d 0, transparent 50%), radial-gradient(at 97% 96%, #8ca8e8 0, transparent 50%), radial-gradient(at 33% 50%, #9772fe 0, transparent 50%), radial-gradient(at 79% 53%, #3a8bfd 0, transparent 50%)',
-            }}
-          ></div>
+            {!isMobileApp && (
+              <div className="sticky bottom-0 pb-6">
+                <div className="mx-auto flex w-full max-w-xl flex-col items-center justify-center gap-2">
+                  <JournalQuickInput />
+                </div>
+              </div>
+            )}
+          </div>
         </div>
+
+        <div
+          className="absolute bottom-0 left-0 right-0 top-0 z-10 opacity-30 dark:opacity-0"
+          style={{
+            filter: 'blur(150px) saturate(150%)',
+            transform: 'translateZ(0)',
+            backgroundImage:
+              'radial-gradient(at 27% 37%, #eea5ba 0, transparent 50%), radial-gradient(at 97% 21%, #fd3a4e 0, transparent 50%), radial-gradient(at 52% 99%, #e4c795 0, transparent 50%), radial-gradient(at 10% 29%, #5afc7d 0, transparent 50%), radial-gradient(at 97% 96%, #8ca8e8 0, transparent 50%), radial-gradient(at 33% 50%, #9772fe 0, transparent 50%), radial-gradient(at 79% 53%, #3a8bfd 0, transparent 50%)',
+          }}
+        ></div>
       </div>
     </>
   )
