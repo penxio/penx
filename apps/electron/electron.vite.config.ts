e1916118645c4285d2ae955e877ca9b53cf7a5bb
0xzio <zio.penx@gmail.com>
2025-07-14 17:55:32 +0800

    chore: improve vite config

--- apps/electron/electron.vite.config.ts
+++ apps/electron/electron.vite.config.ts
@@ -1,40 +1,71 @@
 import { resolve } from 'path'
-import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
-import react from '@vitejs/plugin-react'
 import { lingui } from '@lingui/vite-plugin'
+import react from '@vitejs/plugin-react'
+import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 
 export default defineConfig({
   main: {
-    plugins: [externalizeDepsPlugin()]
+    plugins: [
+      externalizeDepsPlugin({
+        exclude: [
+          '@penx/api',
+          '@penx/components',
+          '@penx/constants',
+          '@penx/content-render',
+          '@penx/contexts',
+          '@penx/domain',
+          '@penx/emitter',
+          '@penx/encryption',
+          '@penx/extension-api',
+          '@penx/hooks',
+          '@penx/icons',
+          '@penx/libs',
+          '@penx/local-db',
+          '@penx/locales',
+          '@penx/model-type',
+          '@penx/novel-editor',
+          '@penx/query-client',
+          '@penx/services',
+          '@penx/session',
+          '@penx/store',
+          '@penx/types',
+          '@penx/uikit',
+          '@penx/unique-id',
+          '@penx/utils',
+          '@penx/vaul',
+          '@penx/widgets',
+        ],
+      }),
+    ],
   },
   preload: {
-    plugins: [externalizeDepsPlugin()]
+    plugins: [externalizeDepsPlugin()],
   },
   renderer: {
     define: {
       'process.env': {},
-      global: {}
+      global: {},
     },
     resolve: {
       alias: {
-        '~': resolve('src/renderer/src')
-      }
+        '~': resolve('src/renderer/src'),
+      },
     },
     plugins: [
       react({
         babel: {
-          plugins: ['@lingui/babel-plugin-lingui-macro']
-        }
+          plugins: ['@lingui/babel-plugin-lingui-macro'],
+        },
       }),
-      lingui()
+      lingui(),
     ],
     build: {
       rollupOptions: {
         input: {
           main: resolve(__dirname, 'src/renderer/index.html'),
-          panel: resolve(__dirname, 'src/renderer/panel.html')
-        }
-      }
-    }
-  }
+          panel: resolve(__dirname, 'src/renderer/panel.html'),
+        },
+      },
+    },
+  },
 })
