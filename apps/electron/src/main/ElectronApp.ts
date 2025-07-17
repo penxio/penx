fe3ec9f366ea21f9baf835c5b2a6ae5a35d7a890
0xzio <zio.penx@gmail.com>
2025-07-18 00:39:14 +0800

    feat: improve dock icon ux

--- apps/electron/src/main/ElectronApp.ts
+++ apps/electron/src/main/ElectronApp.ts
@@ -136,9 +136,14 @@ export class ElectronApp {
       app.on('activate', () => {
         // On macOS it's common to re-create a window in the app when the
         // dock icon is clicked and there are no other windows open.
-        if (BrowserWindow.getAllWindows().length === 0) {
-          this.windows.mainWindow = createMainWindow()
+
+        if (!this.mainWindow) {
+          this.toggleMainWindow()
         }
+
+        // if (BrowserWindow.getAllWindows().length === 0) {
+        //   this.windows.mainWindow = createMainWindow()
+        // }
       })
     } catch (error) {
       console.error('Failed to initialize app:', error)
